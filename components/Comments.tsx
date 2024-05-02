cd79ef78b71e7faa2a4a7460470f685ddaf5ad3a
Shaheer Mansoor <shaheermansoor321@gmail.com>
2024-05-02 19:57:52 +0500

    added: padding to disqus

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -5,18 +5,19 @@ import { useState } from 'react'
 import siteMetadata from '@/data/siteMetadata'
 
 export default function Comments({ slug }: { slug: string }) {
-  const [loadComments, setLoadComments] = useState(false)
+  // const [loadComments, setLoadComments] = useState(false)
 
   if (!siteMetadata.comments?.provider) {
     return null
   }
   return (
-    <>
-      {loadComments ? (
+    <div className="rounded border bg-gray-50 p-4 md:p-8">
+      {/* {loadComments ? (
         <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
       ) : (
         <button onClick={() => setLoadComments(true)}>Load Comments</button>
-      )}
-    </>
+      )} */}
+      <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
+    </div>
   )
 }
