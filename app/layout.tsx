c8ef55ec4142e0f97a475e1052db656922041a3f
Shaheer Mansoor <shaheermansoor321@gmail.com>
2024-05-03 09:55:49 +0500

    test: content={themeColor} />

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,8 +73,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" content={themeColor} />
-      {/* <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
-      <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#030712" /> */}
+      <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
+      <meta name="theme-color" media="(prefers-color-scheme: dark)" content={themeColor} />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
