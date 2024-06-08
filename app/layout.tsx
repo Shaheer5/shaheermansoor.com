146fd470ea7c939b32415f78bcac22d62c82fac1
Shaheer Mansoor <shaheermansoor321@gmail.com>
2024-06-08 13:22:34 +0500

    "Updated Marquee component's text color and favicon assets, including icon images and site.webmanifest file."

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,7 +83,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Marquee
           pauseOnHover
           autoFill
-          className="left-0 top-0 z-50 w-full bg-[#016527] py-1"
+          className="left-0 top-0 z-50 w-full bg-[#016527] py-1 text-white"
           style={{
             position: 'fixed',
           }}
